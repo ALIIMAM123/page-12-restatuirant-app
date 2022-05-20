ad00c8d2c430afec4526df234493063bc873dc42
Md Ali imam <imam.ali1751@gmail.com>
2022-05-20 12:46:13 +0530

    first commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./sidebar.css"
+const Account = () => {
+  return (
+    <div className='sidebar-container'>
+        hello
+    </div>
+  );
+}
+
+export default Account;
